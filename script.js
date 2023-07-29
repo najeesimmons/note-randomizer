7fe22977457ad521bf7a956bd57d1ce0d9a714de
najeesimmons <najee.bryant.simmons@gmail.com>
2023-07-29 18:57:43 -0400

    basic functions working

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 
+// time between random notes in milliseconds
+const duration = 2000;
+let timer;
+
 const pickNote = () => {
-  const index = Math.floor(Math.random() * (notes.length - 1));
-  myNote = notes[index];
+  let index = Math.floor(Math.random() * (notes.length - 1));
   const noteDisplay = document.getElementById("note-display");
+  myNote = notes[index];
   noteDisplay.innerText = myNote;
+};
+
+const start = () => {
+  timer = setInterval(pickNote, duration);
+};
+
+const stop = () => {
+  clearInterval(timer);
+  const noteDisplay = document.getElementById("note-display");
+  noteDisplay.innerText = "";
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   const startButton = document.getElementById("start-button");
-  startButton.addEventListener("click", pickNote);
+  const stopButton = document.getElementById("stop-button");
+  startButton.addEventListener("click", start);
+  stopButton.addEventListener("click", stop);
 });
