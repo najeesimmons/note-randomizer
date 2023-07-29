8af879ef1a587d957db2cbe6045cc92271bed36f
najeesimmons <najee.bryant.simmons@gmail.com>
2023-07-29 16:11:53 -0400

    basic styles done

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 const pickNote = () => {
   const index = Math.floor(Math.random() * (notes.length - 1));
   myNote = notes[index];
-  console.log(myNote);
   const noteDisplay = document.getElementById("note-display");
   noteDisplay.innerText = myNote;
 };
@@ -12,5 +11,3 @@ document.addEventListener("DOMContentLoaded", () => {
   const startButton = document.getElementById("start-button");
   startButton.addEventListener("click", pickNote);
 });
-
-pickNote();
