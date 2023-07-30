294074f2115e0f5b08ab66c8d66f7dbf51b0995d
najeesimmons <najee.bryant.simmons@gmail.com>
2023-07-29 22:00:48 -0400

    add timer

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-
+// duration of exercise
+let startingMinutes = 2;
+let time = startingMinutes * 60;
 // time between random notes in milliseconds
-const duration = 2000;
+const speed = 2000;
+let notePicker;
 let timer;
+
+const updateCountdown = () => {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  clock.innerText = `${minutes}:${seconds}`;
+  time--;
+};
 
 const pickNote = () => {
   let index = Math.floor(Math.random() * (notes.length - 1));
@@ -12,11 +23,13 @@ const pickNote = () => {
 };
 
 const start = () => {
-  timer = setInterval(pickNote, duration);
+  timer = setInterval(updateCountdown, 1000);
+  notePicker = setInterval(pickNote, speed);
 };
 
 const stop = () => {
   clearInterval(timer);
+  clearInterval(notePicker);
   const noteDisplay = document.getElementById("note-display");
   noteDisplay.innerText = "";
 };
@@ -24,6 +37,7 @@ const stop = () => {
 document.addEventListener("DOMContentLoaded", () => {
   const startButton = document.getElementById("start-button");
   const stopButton = document.getElementById("stop-button");
+  const clock = document.getElementById("clock");
   startButton.addEventListener("click", start);
   stopButton.addEventListener("click", stop);
 });
